25bcc9f50f48518a0710567208bfaad00287c9f2
riyasachdeva04 <rsachdeva_be22@thapar.edu>
2023-06-30 20:43:05 +0530

    girlup

--- script.js
+++ script.js
@@ -2,5 +2,20 @@
 window.addEventListener("scroll", function () {
   const parallax = document.querySelector(".parallax");
   let scrollPosition = window.pageYOffset;
-  parallax.style.backgroundPositionY = (scrollPosition * 0.7 + 70)+ "px";
+  parallax.style.backgroundPositionY = (scrollPosition * 0.7 + 70) + "px";
 });
+
+// Smooth scrolling
+const links = document.querySelectorAll('a[href^="#"]');
+for (const link of links) {
+  link.addEventListener('click', scrollSmooth);
+}
+
+function scrollSmooth(e) {
+  e.preventDefault();
+  const target = document.querySelector(this.getAttribute('href'));
+  window.scrollTo({
+    top: target.offsetTop,
+    behavior: 'smooth'
+  });
+}
