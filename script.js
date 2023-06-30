d201ff0ebf38e02bd7162643f423e6c0ca2103e2
riyasachdeva04 <rsachdeva_be22@thapar.edu>
2023-06-30 11:07:59 +0530

    core

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 window.addEventListener("scroll", function () {
     const parallax = document.querySelector(".parallax");
     let scrollPosition = window.pageYOffset;
-    parallax.style.backgroundPositionY = scrollPosition * 0.7 + "px";
+    parallax.style.backgroundPositionY = (scrollPosition * 0.7 + 70)+ "px";
   });
   
